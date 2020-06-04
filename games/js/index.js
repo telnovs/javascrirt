7984cd247e1c974b976dacbc04ca2ba30b4680a2
telnovs <telnov-s@mail.ru>
2020-06-05 00:47:44 +0700

    games_v2

--- games/js/index.js
+++ games/js/index.js
@@ -1,9 +1,27 @@
-let $start = document.querySelector('start')
+/* создаем переменные */
+var $start = document.querySelector('#start')
+var $game = document.querySelector('#game')
 /*стваим обработчик событий на кнопку старт*/
-$start = addEventListener('click', startGame)
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', hedleBoxClick)
 
 /*создаем функцию startGame*/
 function startGame() {
-    console.log('start')
-    $start =classList.add('hide')
+    $game.style.backgroundColor = '#fff'
+    $start.classList.add('hide')
+    /* вызываем фунцию */
+    rendorBox()
+}
+function rendorBox () {
+    var box = document.createElement('div')
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#144122'
+    box.style.top = '50px'
+    box.style.left = '40px'
+    box.style.cursor = 'pointer'
+
+
+    /*ложим данный созданую созданные html  в game */
+    $game.insertAdjacentElement('afterbegin', box)
 }
