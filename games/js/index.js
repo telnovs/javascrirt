a1247e16d884deb1d1e66b4659abd3fb0fcd0432
telnovs <telnov-s@mail.ru>
2020-06-04 23:53:58 +0700

    games_v1

--- games/js/index.js
+++ games/js/index.js
@@ -1,8 +1,9 @@
-let $start = document.querySelector("#start")
+let $start = document.querySelector('start')
 /*стваим обработчик событий на кнопку старт*/
 $start = addEventListener('click', startGame)
 
 /*создаем функцию startGame*/
 function startGame() {
-    console.log("start")
+    console.log('start')
+    $start =classList.add('hide')
 }
