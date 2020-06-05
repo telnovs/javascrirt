69fce734fe5b4eec024a97973869c69cd8e2670d
telnovs <telnov-s@mail.ru>
2020-06-05 23:56:16 +0700

    games_v4

--- games/js/index.js
+++ games/js/index.js
@@ -1,24 +1,67 @@
 /* создаем переменные */
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
+var $time = document.querySelector('#time')
+var $result = document.querySelector('#result')
+var isGameStarted = false
+var $timeHeader = document.querySelector('#time-header')
+var $resultHeader = document.querySelector('#result-header')
 
-var store = 0
+var score = 0
 /*стваим обработчик событий на кнопку старт*/
 $start.addEventListener('click', startGame)
 /*Ставим прослушку события*/
 $game.addEventListener('click', hedleBoxClick)
 
 /*создаем функцию startGame*/
+/* запуск игры*/
 function startGame() {
+    score = 0
+    setGameTime()
+    $timeHeader.classList.remove('hide')
+    $resultHeader.classList.add('hide')
+    isGameStarted = true
     $game.style.backgroundColor = '#fff'
     $start.classList.add('hide')
+    /*Создаем интервал */
+    let interval = setInterval(function(){
+        let time = parseFloat($time.textContent)
+        if (time <=0){
+            clearInterval(interval)
+            endGame()
+        }else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    },100)
     /* вызываем фунцию */
     rendorBox()
 }
 /* Создаем функции*/
+function setGameTime(){
+    let time = 5
+    $time.textContent = time.toFixed(1)
+}
+function setGameScore(){
+    $result.textContent = score.toString()
+}
+
+function endGame(){
+    isGameStarted = false
+    setGameScore()
+    $start.classList.remove('hide')
+    $game.innerHTML = ''
+    $game.style.backgroundColor = '#ccc'
+    $timeHeader.classList.add('hide')
+    $resultHeader.classList.remove('hide')
+
+}
+
 function hedleBoxClick(event){
+    if(!isGameStarted){
+        return
+    }
     if(event.target.dataset.box){
-        store ++
+        score ++
         rendorBox ()
     }
 }
