bd7ceb5738054e4e9b32bccf2fea7c55b1fdf619
telnovs <telnov-s@mail.ru>
2020-06-05 22:11:40 +0700

    games_v3_getRandom

--- games/js/index.js
+++ games/js/index.js
@@ -1,8 +1,11 @@
 /* создаем переменные */
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
+
+var store = 0
 /*стваим обработчик событий на кнопку старт*/
 $start.addEventListener('click', startGame)
+/*Ставим прослушку события*/
 $game.addEventListener('click', hedleBoxClick)
 
 /*создаем функцию startGame*/
@@ -12,16 +15,35 @@ function startGame() {
     /* вызываем фунцию */
     rendorBox()
 }
+/* Создаем функции*/
+function hedleBoxClick(event){
+    if(event.target.dataset.box){
+        store ++
+        rendorBox ()
+    }
+}
 function rendorBox () {
+
+    $game.innerHTML = ''
     var box = document.createElement('div')
-    box.style.height = box.style.width = '50px'
+    let boxSize = getRandom(20,150)// создаем переменную с функцие которая рандомит числа в заданном диапозоне
+    let gameSize = $game.getBoundingClientRect()// создаем переменную присваеваем ей значение
+    //функции getBoundingClientRect с помощью которой определяем размеры поля game
+    let maxTop = gameSize.height - boxSize
+    let maxLeft = gameSize.width - boxSize
+    box.style.height = box.style.width = boxSize +'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#144122'
-    box.style.top = '50px'
-    box.style.left = '40px'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
-
+    box.setAttribute('data-box', 'true')
 
     /*ложим данный созданую созданные html  в game */
     $game.insertAdjacentElement('afterbegin', box)
+}
+/*Создаем функцию для создание случайных чисель */
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
 }
