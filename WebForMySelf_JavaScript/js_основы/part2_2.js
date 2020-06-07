5703c3f30e186a858d5acc3b785704b02e4da531
telnovs <telnov-s@mail.ru>
2020-06-07 21:04:30 +0700

    part2_2_типы_данных

---
+++ WebForMySelf_JavaScript/js_основы/part2_2.js
@@ -0,0 +1,23 @@
+/* типы данных
+  Number
+  String
+  Boolean
+  Object
+  Null
+  Undefined
+*/
+
+let num = 42;
+let string = 'Message'
+let isTrue = false // false
+let obj = {a: 1}
+let nothing = null
+let undef = undefined
+
+// поверка  типа с помощью оператора typeof
+console.log(typeof num)
+console.log(typeof string)
+console.log(typeof isTrue)
+console.log(typeof obj)
+console.log(typeof nothing)
+console.log(typeof undef)
