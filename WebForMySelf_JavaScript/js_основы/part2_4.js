f9daff0a28b9ad422780f0dff611329489987f53
telnovs <telnov-s@mail.ru>
2020-06-07 21:35:59 +0700

    part2_4+5_Приоритет_операции

---
+++ WebForMySelf_JavaScript/js_основы/part2_4.js
@@ -0,0 +1,29 @@
+let result = 12 - 6 / 3
+let result2 = (3 + 4) * 2 // 4 * 2 = 8, 3 + 8 = 11
+
+let isGreater = 20 - 6 * 3 >= 4
+let isGreater2 = 20 - 6 * 3 >= 1
+//             3    13  14  11
+
+console.log('12 - 6 / 3', result)
+console.log('(3 + 4) * 2', result2)
+console.log('20 - 6 * 3 >= 4', isGreater)
+console.log('20 - 6 * 3 >= 1', isGreater2)
+
+//остаток от деление %
+console.log('5 % 2', 5 % 2)
+console.log('8 % 3', 8 % 3)
+console.log('15 % 5', 15 % 5)
+
+let i = 1
+// i = i + 1
+i++
+console.log('i', i)
+// i = i + 3
+i *= 3
+console.log('i', i)
+i /= 1.5
+console.log('i', i)
+
+console.log('i', ++i)
+console.log('i', i--)
