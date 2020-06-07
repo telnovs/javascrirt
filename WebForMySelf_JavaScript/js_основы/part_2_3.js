4dc03c0a5c7e89f223481a76fb97e183b4d7bea7
telnovs <telnov-s@mail.ru>
2020-06-07 21:14:22 +0700

    part2_3_Базовые_операции

---
+++ WebForMySelf_JavaScript/js_основы/part_2_3.js
@@ -0,0 +1,20 @@
+let num1 = 12
+let num2 = 8
+
+console.log('+:', num1 + num2)
+console.log('-:', num1 - num2)
+console.log('*:', num1 * num2)
+console.log('/:', num1 / num2)
+
+let str1 = 'Hello'
+let str2 = 'World'
+
+console.log('+:', str1 + ' ' + str2)
+
+console.log('1 + 2:', 1 + '2')
+console.log('str1 + num1:', str1 + num1)
+
+console.log('Boolean + string:', true + str2)
+
+console.log('True + 1:', true + 1)
+console.log('False + 1:', false + 1)
