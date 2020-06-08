6d6d849ea206a70b44c075ab9adc426db32d6dab
telnovs <telnov-s@mail.ru>
2020-06-08 21:23:38 +0700

    part2_11_function

--- WebForMySelf_JavaScript/js_основы/part2_11.js
+++ WebForMySelf_JavaScript/js_основы/part2_11.js
@@ -2,19 +2,20 @@
  let personYear = 1990
     // cоздание функции, функция принимает параметр на входе
     //
- function calcileteAge(year) {
+ function calcileteAge(age) {
     let currentYear = 2020           // создание переменной в функции
-    let result  = currentYear - year // вечесляем из переменной входящий параметр
+    let result  = currentYear - age // вечесляем из переменной входящий параметр
     return result                   // возвращем полученный результат
  }
 
- function checkAngLodgAdge(year) {
-    if(calcileteAge(year) < 10){
-        console.log('Возраст меньше 10 лет')
+ function checkAngLodgAdge(year, name ,compareTo ) {
+    if(calcileteAge(year) < compareTo){
+        console.log('Возраст ' +name + ' меньше '+compareTo+' лет')
     }else {
-        console.log('Возраст больше 10 лет')
+        console.log('Возраст ' +name + ' больше '+compareTo+' лет')
      }
  }
 
- checkAngLodgAdge(carYear)
- checkAngLodgAdge(personYear)
+ checkAngLodgAdge(carYear,'ford', 20 )
+ checkAngLodgAdge(personYear,'Victor',50 )
+ calcileteAge(58)
