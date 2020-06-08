6aa933ff66996dd9a9b97e10f8af96b45f8f6289
telnovs <telnov-s@mail.ru>
2020-06-08 17:54:59 +0700

    part2_9_тенарный_оператор

---
+++ WebForMySelf_JavaScript/js_основы/part2_9.js
@@ -0,0 +1,39 @@
+let currentYear = 2018
+let carName = 'Ford'
+let carYear = 2014
+let carAge = currentYear - carYear
+
+// если в.м. меньше 5 лет ...
+// если в.м. больше или равно 5 лет и меньше или равно 10 лет ...
+// иначе другое сообщение
+
+if (carAge < 5) {
+    console.log(carName + ' младше 5 лет')
+} else if (carAge >= 5 && carAge <= 10) {
+    console.log(carName + ' больше или равен 5 годам или меньше или равен 10 годам')
+} else {
+    console.log('Возраст ' + carName + ' равняется ' + carAge + ' годам')
+}
+
+// 0 null undefined '' NaN  значение при проверке будет false
+
+let str = 'Hello'
+
+if (null) {
+    console.log('Значение true')
+} else {
+    console.log('Значение false')
+}
+
+
+
+
+let personAge = 28
+// тернароное вырожение
+let message = personAge < 18                 //  условие
+    ? 'Человек еще не совершеннолетний'      // если true то выполняеться это блок
+    : 'Человек совершеннолетний'            // если false то выполняеться этот блок
+
+console.log(message)
+
+
