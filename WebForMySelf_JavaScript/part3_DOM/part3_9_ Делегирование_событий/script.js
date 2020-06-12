d19b93d6f87f04d8f25958b956b4eb37033d3ab9
telnovs <telnov-s@mail.ru>
2020-06-12 12:15:18 +0700

    part3_9_Делегирование_событий

--- WebForMySelf_JavaScript/part3_DOM/part3_9_ Делегирование_событий/script.js
+++ WebForMySelf_JavaScript/part3_DOM/part3_9_ Делегирование_событий/script.js
@@ -1,20 +1,22 @@
-// var p = document.querySelectorAll('p')
+// let p = document.querySelectorAll('p') // забираем элемент
 
-// for (var i = 0; i < p.length; i++) {
-//   p[i].addEventListener('click', function(event) {
-//     event.target.style.color = 'blue'
-//   })
+// for (var i = 0; i < p.length; i++) {// проходимся по всем элементам через  цикл
+//   p[i].addEventListener('click', function(event) { // присваеваем каждому элементу клик и фунцию
+//     event.target.style.color = 'blue'//через target добовляем стиль элементу
+//   })// получаеться если кликнуть по элементу он поменяет цвет.
 // }
 
-
+// берем элемент wrapper создаем  событие клик по нему, добавляем анонимную функцию с параметром event
 document.getElementById('wrapper').addEventListener('click', function(event) {
-    var tagName = event.target.tagName.toLowerCase()
-
+    // создаем переменную tagName и присваем ей название тега (event.target.tagName )в нижнем регистре toLowerCase
+    let tagName = event.target.tagName.toLowerCase()
+// создаем условия
+// если переменная совпадает с тегом параграф (р)
     if (tagName === 'p') {
-        event.target.style.color = 'blue'
+        event.target.style.color = 'blue' // присваеваем голубой цвет
     }
-
+//создаем условия: если у нас в элементе есть класс ('color')
     if (event.target.classList.contains('color')) {
-        event.target.style.color = 'red'
+        event.target.style.color = 'red'// присваеваем красный цвет
     }
 })
