36a409da3458c286ef5f931551f0b22a8a3b8874
telnovs <telnov-s@mail.ru>
2020-06-12 13:10:54 +0700

    part4_2_ Взаимодействие_с_пользователем

---
+++ WebForMySelf_JavaScript/part4_Дополнительные_возможности/part4_2_Взаимодействие_с_пользователем/script.js
@@ -0,0 +1,21 @@
+//alert просто сплываеющие окно с одной кнопкой
+document.querySelector('#alert').addEventListener('click', function(event){
+     alert('вы нажали на кнопку')
+ })
+// элемент confirm выводит окошко с двумя знамчениями
+ document.querySelector('#confirm').addEventListener('click', function(event){
+    // создаем переменную с которой в дальншем можем работать в зависимости от выбора пользователя
+    let decision = confirm('нажмите что ни будь')
+
+     if (decision){
+         alert('вы нажали на кнопку')
+     }
+ })
+// prompt окно с полем вода пользователя, есть поле по умолчанию
+document.querySelector('#prompt').addEventListener('click', function(event) {
+   let userName = prompt('Our name???', '')
+//делаем проверку если пользователь ввел значение вывожим его в alert
+    if (userName != null) {
+        alert( 'Hello ' + userName)
+    }else prompt('Our name???')
+})
