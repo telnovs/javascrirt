384c961434a615ea263cd1431990baca3f2a4f6b
telnovs <telnov-s@mail.ru>
2020-06-12 15:10:42 +0700

    part4_4_Math

---
+++ WebForMySelf_JavaScript/part4_Дополнительные_возможности/part4_4_Math/script.js
@@ -0,0 +1,6 @@
+//случайное число
+console.log(Math.random())
+ let num = 2.4
+// округление числа
+console.log(Math.floor(num)) // всегда округляет в меньшую сторону
+console.log(Math.ceil(num)) // всегда округляет в большую сторону
