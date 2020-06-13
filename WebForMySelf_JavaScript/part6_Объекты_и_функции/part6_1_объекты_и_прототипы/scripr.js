964908c8e1ee813bfd7e10cd00e2422ee7ab8d4e
telnovs <telnov-s@mail.ru>
2020-06-13 23:28:06 +0700

    part6_1_объекты_и_прототипы

---
+++ WebForMySelf_JavaScript/part6_Объекты_и_функции/part6_1_объекты_и_прототипы/scripr.js
@@ -0,0 +1,17 @@
+// number, string, boolean, null, undefined - тип данных. примитивы
+// object - все в js является обьектом. тоже тип данных
+
+
+// быстрй метод создание обьекта
+let car = {
+    name: 'Ford',
+    year: 2015,
+    person: {
+
+    }
+}
+//цепочка наследование
+// car.__proto__ => Object.prototype
+// [] => Array => Object
+
+console.log(car)
