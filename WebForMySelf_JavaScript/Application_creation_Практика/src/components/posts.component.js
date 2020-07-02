336874a37c8188221417acc17d8a4d64cbf3620f
telnovs <telnov-s@mail.ru>
2020-07-02 20:32:03 +0700

    Application_creation_Вывод_списка_постов_урок14

--- WebForMySelf_JavaScript/Application_creation_Практика/src/components/posts.component.js
+++ WebForMySelf_JavaScript/Application_creation_Практика/src/components/posts.component.js
@@ -8,6 +8,42 @@ export class PostsComponent extends Component {
   }
   async onShow(){
    const fbdata = await apiService.fetchPosts()
+    // для вывода обьетка с сервеса
     const posts = TransformService.fbObjectToArray(fbdata)
+    //для вывода поста
+    const html =posts.map(post =>renderPost(post))
+    this.$el.insertAdjacentHTML('afterbegin',html.join(' '))
   }
+  //очищаем html  когда уходим с данной компаненты с помощью метода onHide
+  onHide() {
+   this.$el.innerHTML = ''
+  }
+}
+
+// создаем метод который будет возвращать html раметку
+//на входе он пролучет обьект поста
+function renderPost(post){
+  // оздаем переменную и проверяем сразу тернарным вырожением
+  const tag = post.type === 'news'
+      ?  '<li class="tag tag-blue tag-rounded">Новость</li>'
+      :  '<li class="tag tag-rounded">Заметка</li>'
+  const button = '<button class="button-round button-small button-primary">Сохранить</button>'
+  //возвращаем html раметку
+  return `
+    <div class="panel">
+        <div class="panel-head">
+          <p class="panel-title">${post.title}/p>
+          <ul class="tags">
+           ${tag}
+          </ul>
+        </div>
+        <div class="panel-body">
+          <p class="multi-line">${post.fulltext}</p>
+        </div>
+        <div class="panel-footer w-panel-footer">
+          <small>${post.data}</small>
+          ${button}
+        </div>
+     </div>
+  `
 }
