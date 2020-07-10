6304630ae8a949ba1ecc5c0350900a75a6201c26
telnovs <telnov-s@mail.ru>
2020-07-11 00:32:47 +0700

    appV2-css

--- WebForMySelf_JavaScript/nodeJS/index.js
+++ WebForMySelf_JavaScript/nodeJS/index.js
@@ -3,9 +3,16 @@ const express = require('express')
 
 //создаем переменную которая отвечает за все приложение
 const app = express()
+//говорим что по умолчанию у нас идут файлы ejs
+app.set('view engine','ejs')
 
+//подключем css
+app.use(express.static('public'))
+
+// при запросе на корневую страницу нашего приложения методом get мы будем рендорить файл
+// index с раширением ejs
 app.get('/',(req, res) => {
-    res.end('Hello from node js')
+    res.render('index')
 })
 
 //создаем функцию  listen где первый значением передаем порт на котором слушать приложение
