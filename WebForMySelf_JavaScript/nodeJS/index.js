f8b0aa1ae6a748fc9ce8ede1b5d64cf681e24c2e
telnovs <telnov-s@mail.ru>
2020-07-11 11:41:56 +0700

    appV3-обработка-формы

--- WebForMySelf_JavaScript/nodeJS/index.js
+++ WebForMySelf_JavaScript/nodeJS/index.js
@@ -1,5 +1,7 @@
 // подключаем express
 const express = require('express')
+// подключаем body-parser
+const bodyParser = require('body-parser')
 
 //создаем переменную которая отвечает за все приложение
 const app = express()
@@ -8,11 +10,26 @@ app.set('view engine','ejs')
 
 //подключем css
 app.use(express.static('public'))
+//добовляем функционал нашему приложению для получения данных в request.body
+app.use(bodyParser.urlencoded({extended: true}))
 
 // при запросе на корневую страницу нашего приложения методом get мы будем рендорить файл
 // index с раширением ejs
 app.get('/',(req, res) => {
+
     res.render('index')
+})
+
+//обработка POST запроса
+//request - отвечает за то что мы отправляем(обращаемся) в данном запросе
+//response - отвечает за то что мы отвечаем на данный зарпрос
+app.post('/',(request,response) =>{
+    //получение значения инпута
+    const { city } = request.body
+
+    console.log(city)
+    // возвращаем туже страеицу но с изменнемыми параметрами
+    response.render('index')
 })
 
 //создаем функцию  listen где первый значением передаем порт на котором слушать приложение
