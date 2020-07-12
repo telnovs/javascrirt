2cc2c920907832afa0d3c37c4a21e216dcc1bf10
telnovs <telnov-s@mail.ru>
2020-07-12 22:11:01 +0700

    WepackV3-Подключение-плагинов

--- WebForMySelf_JavaScript/WebPack4/webpack.config.js
+++ WebForMySelf_JavaScript/WebPack4/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path') /* используем встроенный модуль node js который работает с путями */
 
+/* Подключаем html-webpack-plugin*/
+const HTMLPlugin = require('html-webpack-plugin')
+
+
 module.exports = {
     entry: './src/index.js',  /*входной файл*/
     output: { /*выходной фаил из webpack*/
         filename: 'bundle.js', /* название файла , обычно bundle.js*/
         path: path.resolve(__dirname, 'dist')/* указываем путь */
-    }
+    },
+    /* Подключаем плаген путем создание массива */
+    plugins:[
+        /* Плагин подключется путом создание нового класса плагина */
+        new HTMLPlugin({
+            filename: 'index.html',
+            /* берем за основу html файл для обработки этого файла*/
+            template: './src/index.html'
+        })
+    ]
 }
