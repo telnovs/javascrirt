e807816541dc918e98d7cf1fac1d9bbe417b2c11
telnovs <telnov-s@mail.ru>
2020-07-12 22:45:41 +0700

    WepackV4-Работа-библиотеками

--- WebForMySelf_JavaScript/WebPack4/src/modules/heared.component.js
+++ WebForMySelf_JavaScript/WebPack4/src/modules/heared.component.js
@@ -1,1 +1,14 @@
-console.log('Hello webpack!!!')
+//подключаем jquery
+import  $ from 'jquery'
+
+//создаем заголовок с промощью jquery
+$('<h1 />')
+    // передаем текст в заголовок
+    .text('Hello jquery')
+    // подключим  стили
+    .css({
+        textAlign: 'center',
+        color: 'blue'
+    })
+    //ставляем текст в хедер
+    .appendTo($('header'))
