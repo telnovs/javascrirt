1a468761d2f8dcbeb1907289223983dc7c15619a
telnovs <telnov-s@mail.ru>
2020-07-18 23:57:48 +0700

    Object

---
+++ JavaScriptMinin/Object/index.js
@@ -0,0 +1,41 @@
+// создаем новый обьект через Object.create  
+// метод create принимает 2 параметра и оба из них являются обьектами 
+// первый обьект являеться прототипом для данного обьекта 
+// во втором мы можем указать определенные поля для даного обьекта 
+
+const person = Object.create ({}, {
+	 name: { // поле тут являеться обьектом  
+	 	value: 'Mark',  // ключ создаестья  через value
+	 	// что бы видно было поле name в цикле for нужен параметр enumerable, по умолчанию он идет как folse.
+	 	// необходимо его сдеалть как true
+	 	enumerable: true ,
+	 	// writable  говорит что нельзя заменять поля, по умолчанию в значении false
+	 	writable: true,
+	 	// configurable  данный параметр говорит что мы можем удалять какой либо ключ из обьетка, по умолчанию false
+	 	// удаляться с помощью оператора  delete  
+	 	configurable : true 
+	 },
+	 birdYear: {
+	 	value: 1986  ,
+	 	enumerable: false,
+	 	writable:false ,
+	 	configurable : true 
+	 },
+	 age: {
+	 	get(){ // в get  должны вернуть новое значение  
+	 		return new Data().getFullYear()- this.birdYear
+	 	},
+	 	set(value){
+	 		document.body.style.background = 'blue'
+	 		console.log('Set age', value )
+
+	 	}
+	 }
+	 
+})  
+
+// person.name = "Blad"
+
+for (let key in person){
+	console.log('key:', key , person[key])// что бы получить значения ключа необходимо написать person[key] 
+ }
