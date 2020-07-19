8f4a711612ab38111e85eb206ec2222237bb9272
telnovs <telnov-s@mail.ru>
2020-07-19 20:35:39 +0700

    Object

--- JavaScriptMinin/Object/index.js
+++ JavaScriptMinin/Object/index.js
@@ -3,7 +3,13 @@
 // первый обьект являеться прототипом для данного обьекта 
 // во втором мы можем указать определенные поля для даного обьекта 
 
-const person = Object.create ({}, {
+const person = Object.create ({
+
+	calculateAge(){
+		console.log('Age:',new Data().getFullYear()- this.birdYear
+	 	},)
+	}
+}, {
 	 name: { // поле тут являеться обьектом  
 	 	value: 'Mark',  // ключ создаестья  через value
 	 	// что бы видно было поле name в цикле for нужен параметр enumerable, по умолчанию он идет как folse.
@@ -26,7 +32,7 @@ const person = Object.create ({}, {
 	 		return new Data().getFullYear()- this.birdYear
 	 	},
 	 	set(value){
-	 		document.body.style.background = 'blue'
+	 		document.body.style.background = 'blue '
 	 		console.log('Set age', value )
 
 	 	}
@@ -36,6 +42,9 @@ const person = Object.create ({}, {
 
 // person.name = "Blad"
 
-for (let key in person){
-	console.log('key:', key , person[key])// что бы получить значения ключа необходимо написать person[key] 
- }
+for (let key in person
+	//добовляем проверку если у обькта person
+	if (person.hesOwProperty(key)){
+		console.log('key:', key , person[key])// что бы получить значения ключа необходимо написать person[key] 
+	}
+	
