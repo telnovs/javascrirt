a2d3850a14e871b968a8b75b1b49e62767d2d80d
telnovs <telnov-s@mail.ru>
2020-07-24 23:15:05 +0700

    Array Methods

---
+++ JavaScriptMinin/ArrayMethods/index.js
@@ -0,0 +1,14 @@
+const people = [
+  {  name: 'Vladilen',  age: 25, budget: 4000 },
+  {  name: 'Elena',  age: 22 ,budget: 4100 },
+  {  name: 'Victor', age: 23, budget: 4200 },
+  {  name: 'Vasilisa', age: 25, budget: 4300 },
+  {  name: 'Mark',  age: 26, budget: 4400 },
+  {  name: 'Ivan', age: 27, budget: 44500 },
+  {  name: 'Виктор',  age: 28, budget: 4600 }
+]
+
+for (let i =0; i <people.length; i++){
+	console.log(people[i])
+}
+
